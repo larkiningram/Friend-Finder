06886e6867aa72549b1622c35191141b66f49103
Larkin <larkiningram@gmail.com>
2019-10-13 12:31:31 -0500

    added page to view all friends

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,4 +8,8 @@ module.exports = function (app) {
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
+
+    app.get("/friends", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/friends.html"));
+    });
 };
